ca525f2c85db7ce720a879a7bb235c77579f4eba
Antigen-1 <zhanghao@antigen.top>
2023-10-02 11:34:44 +0800

    Fix info.rkt

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "racket-gsl")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "expeditor-lib"))
 (define scribblings '(("scribblings/racket-gsl.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
