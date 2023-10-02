cedef8428ab7a143d0d5b3a6e9682761b9346c10
Antigen-1 <zhanghao@antigen.top>
2023-10-02 11:02:02 +0800

    Fix library.rkt

--- library.rkt
+++ library.rkt
@@ -1,11 +1,11 @@
 #lang racket/base
 (require racket/runtime-path ffi/unsafe setup/dirs (for-syntax racket/base))
-(provide libgslblas libgsl)
+(provide libgslcblas libgsl)
 
 (define-runtime-path lib-dir (build-path "gsl-fork" "build" "lib"))
 
-(define (find-ffi-lib name version)
-  (ffi-lib name (list version #f) #:get-lib-dirs (lambda () (cons lib-dir (get-lib-search-dirs)))))
+(define (find-ffi-lib name version #:global? (global? #f))
+  (ffi-lib name (list version #f) #:global? global? #:get-lib-dirs (lambda () (cons lib-dir (get-lib-search-dirs)))))
 
-(define libgslblas (find-ffi-lib "libgslcblas" "0.0.0" #:global? #t))
+(define libgslcblas (find-ffi-lib "libgslcblas" "0.0.0" #:global? #t))
 (define libgsl (find-ffi-lib "libgsl" "25.1.0"))
